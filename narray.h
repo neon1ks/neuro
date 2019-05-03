5f1ed7295e0f06dcf186c6048e63560a46d620c8
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2019-05-03 11:08:59 +0800

    Добавлен код класса NArray - Часть 2

--- narray.h
+++ narray.h
@@ -1,11 +1,39 @@
 #ifndef NARRAY_H
 #define NARRAY_H
 
-
-class NArray
-{
-public:
+template <typename NType>
+class NArray {
+   public:
 	NArray();
+	NArray(NArray<NType>& obj);  ///< Конструктор копирования
+	explicit NArray(int size);
+	~NArray();
+
+   protected:
+	int m_size;     ///< Размер доступной памяти
+	int m_lenght;   ///< Длина реальных данных
+	int m_block;    ///< Шаг изменения размера данных
+	NType* m_data;  ///< Указатель на хранящиеся данные
+
+   public:
+	void init(int lenght, NType& value);
+	void add(NType element, int pos);
+	NType del(int pos);
+
+	void push(NType element);
+	NType pop();
+
+	void set(NType element, int pos);
+	NType get(int pos);
+
+	void setBlock(int block);
+
+	int getSize();
+	int getLenght();
+	int getBlock();
+
+	void resize(int size);
+	NType* getData();
 };
 
-#endif // NARRAY_H
+#endif  // NARRAY_H
