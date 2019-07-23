aa5136e240908d5cc37574e61b1ea68074b125be
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2019-07-23 14:39:34 +0800

    Реализация класса NLayerArctg<NType>

--- nlayeracrtg.h
+++ nlayeracrtg.h
@@ -1,4 +1,37 @@
 #ifndef NLAYERACRTG_H
 #define NLAYERACRTG_H
 
-#endif // NLAYERACRTG_H
+#include <cmath>
+#include "inlayer.h"
+
+template <typename NType>
+class NLayerArctg : public INLayer<NType> {
+   public:
+	NLayerArctg();
+	virtual ~NLayerArctg();
+
+   public:
+	virtual NType activation(NType& x) override;
+	virtual NType derivative(NType& y) override;
+};
+
+template <typename NType>
+NLayerArctg<NType>::NLayerArctg() {
+	this->m_koef = 2;
+}
+
+template <typename NType>
+NLayerArctg<NType>::~NLayerArctg() {
+}
+
+template <typename NType>
+NType NLayerArctg<NType>::activation(NType& x) {
+	return atan(this->m_koef * x);
+}
+
+template <typename NType>
+NType NLayerArctg<NType>::derivative(NType& y) {
+	return this->m_koef / (pow(this->m_koef * y, 2) + 1);
+}
+
+#endif  // NLAYERACRTG_H
