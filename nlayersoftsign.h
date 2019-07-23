65a4cd854f173f3e894d0ed79b8e9aa2a2cfea8c
Maksim Demyanov (neon1ks) <neon1ks@mail.ru>
2019-07-23 11:57:10 +0800

    Реализация класса NLayerSoftsign<NType>

--- nlayersoftsign.h
+++ nlayersoftsign.h
@@ -1,4 +1,37 @@
 #ifndef NLAYERSOFTSIGN_H
 #define NLAYERSOFTSIGN_H
 
-#endif // NLAYERSOFTSIGN_H
+#include <cmath>
+#include "inlayer.h"
+
+template <typename NType>
+class NLayerSoftsign : public INLayer<NType> {
+   public:
+	NLayerSoftsign();
+	virtual ~NLayerSoftsign();
+
+   public:
+	virtual NType activation(NType& x);
+	virtual NType derivative(NType& y);
+};
+
+template <typename NType>
+NLayerSoftsign<NType>::NLayerSoftsign() {
+	this->m_koef = 1;
+}
+
+template <typename NType>
+NLayerSoftsign<NType>::~NLayerSoftsign() {
+}
+
+template <typename NType>
+NType NLayerSoftsign<NType>::activation(NType& x) {
+	return x / (1 / this->m_koef + abs(x));
+}
+
+template <typename NType>
+NType NLayerSoftsign<NType>::derivative(NType& y) {
+	return this->m_koef / pow(1 + this->m_koef * abs(y), 2);
+}
+
+#endif  // NLAYERSOFTSIGN_H
